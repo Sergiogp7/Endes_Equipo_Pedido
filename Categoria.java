c3b9ac6e503d41d2ed4c8b66dad0e055686abbce
raulvt02 <vera.trashorras.raul@iescamas.es>
2025-03-28 10:46:37 +0100

    Creacion clases Producto,Categoria y Reseña

---
+++ Categoria.java
@@ -0,0 +1,48 @@
+
+
+/**
+ * Class Categoria
+ */
+public class Categoria {
+
+  //
+  // Fields
+  //
+
+  private String nombre;
+  
+  //
+  // Constructors
+  //
+  public Categoria () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of nombre
+   * @param newVar the new value of nombre
+   */
+  public void setNombre (String newVar) {
+    nombre = newVar;
+  }
+
+  /**
+   * Get the value of nombre
+   * @return the value of nombre
+   */
+  public String getNombre () {
+    return nombre;
+  }
+
+  //
+  // Other methods
+  //
+
+}
---
+++ Reseña.java
@@ -0,0 +1,82 @@
+
+
+/**
+ * Class Reseña
+ */
+public class Reseña {
+
+  //
+  // Fields
+  //
+
+  private int id;
+  private String comentario;
+  private int puntaje;
+  
+  //
+  // Constructors
+  //
+  public Reseña () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of id
+   * @param newVar the new value of id
+   */
+  public void setId (int newVar) {
+    id = newVar;
+  }
+
+  /**
+   * Get the value of id
+   * @return the value of id
+   */
+  public int getId () {
+    return id;
+  }
+
+  /**
+   * Set the value of comentario
+   * @param newVar the new value of comentario
+   */
+  public void setComentario (String newVar) {
+    comentario = newVar;
+  }
+
+  /**
+   * Get the value of comentario
+   * @return the value of comentario
+   */
+  public String getComentario () {
+    return comentario;
+  }
+
+  /**
+   * Set the value of puntaje
+   * @param newVar the new value of puntaje
+   */
+  public void setPuntaje (int newVar) {
+    puntaje = newVar;
+  }
+
+  /**
+   * Get the value of puntaje
+   * @return the value of puntaje
+   */
+  public int getPuntaje () {
+    return puntaje;
+  }
+
+  //
+  // Other methods
+  //
+
+}
